728e7b4716e8db9494a705beaeb8ac332487ab2f
kibrsifr <kiaobrent@gmail.com>
2025-05-13 19:35:07 +0800

    Tilemap Test Sorting

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,7 +9,7 @@ func _ready():
 func _physics_process(delta):
 	player_movement(delta)	
 	
-func player_movement(delta):
+func player_movement(_delta):
 	
 	if Input.is_action_pressed("ui_right"):
 		current_dir = "right"
